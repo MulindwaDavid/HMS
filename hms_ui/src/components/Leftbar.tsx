fec80f8071fa3f0a434989c8d99deaf56efe1169
Mulindwa David <david@david.168.1.115>
2026-03-12 17:14:20 +0300

    some updates

--- hms_ui/src/components/Leftbar.tsx
+++ hms_ui/src/components/Leftbar.tsx
@@ -1,27 +1,43 @@
-import React from "react";
+import type { IconType } from "react-icons";
+import { MdDashboard } from "react-icons/md";
+import { FaBed } from "react-icons/fa";
+import { HiOutlineDocumentReport } from "react-icons/hi";
+import { MdPayments } from "react-icons/md";
+import { FaCalendarCheck } from "react-icons/fa";
+import { FaCommentDots } from "react-icons/fa";
+import { FaBox } from "react-icons/fa";
+
+import { MdSettings } from "react-icons/md";
+import { MdHistory } from "react-icons/md";
+import { MdSecurity } from "react-icons/md";
+import { MdHelpOutline } from "react-icons/md";
+
+import { NavLink } from "react-router-dom";
+
 
 interface SidebarItem {
   label: string;
-  icon: string;
+  icon: IconType;
+  path: string;
 }
 
 function Leftbar() {
 
   const items: SidebarItem[] = [
-    { label: "Overview", icon: "#" },
-    { label: "Rooms", icon: "#" },
-    { label: "Reports", icon: "#" },
-    { label: "Payments", icon: "#" },
-    { label: "Reservations", icon: "#" },
-    { label: "Comments", icon: "#" },
-    { label: "Inventory", icon: "#" }
+    { label: "Overview", icon: MdDashboard, path: "/overview" },
+    { label: "Rooms", icon: FaBed, path: "/rooms" },
+    { label: "Reports", icon: HiOutlineDocumentReport, path: "/reports" },
+    { label: "Payments", icon: MdPayments, path: "/payments" },
+    { label: "Reservations", icon: FaCalendarCheck, path: "/reservations" },
+    { label: "Comments", icon: FaCommentDots , path: "/comments"},
+    { label: "Inventory", icon: FaBox, path: "/inventory"}
   ];
 
   const settingsItem: SidebarItem[] = [
-    {label:"System configuration", icon: "#"},
-    {label:"Activity Log", icon: "#"},
-    {label:"Permissions", icon: "#"},
-    {label:"Help", icon: "#"}
+    {label:"System configuration", icon: MdSettings, path: "/settings"},
+    {label:"Activity Log", icon: MdHistory, path: "/history"},
+    {label:"Permissions", icon: MdSecurity, path: "/security"},
+    {label:"Help", icon: MdHelpOutline, path: "/help"}
   ];
 
   return (
@@ -42,6 +58,8 @@ function Leftbar() {
 
       {items.map(function(item, index) {
 
+        const Icon = item.icon;
+
         return (
           <div
             key={index}
@@ -54,10 +72,13 @@ function Leftbar() {
 
             className="sidebar-item"
           >
-            <button className="sidebar-btn">
-            <img src={item.icon} alt="icon" />
+            
+            <NavLink to={item.path}
+              className={({ isActive}) => isActive ? "sidebar-btn active" : "sidebar-btn"}
+            >
+             <Icon size={18} />
             <span>{item.label}</span>
-            </button>
+            </NavLink>
           </div>
         );
       })}
@@ -66,6 +87,8 @@ function Leftbar() {
       <h3 style={{color: "#1e4ed8"}}> SETTINGS</h3>
 
         {settingsItem.map(function(item, index) {
+
+          const Icon = item.icon;
 
           return(
 
@@ -79,7 +102,7 @@ function Leftbar() {
             >
 
               <button className="sidebar-btn">
-              <img src={item.icon} alt="icon" />
+               <Icon size={18} />
               <span>{item.label}</span>
               </button>
             </div>
